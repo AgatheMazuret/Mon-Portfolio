df09ba1b9c7bded0ced3dcdbc4dc326be88c869e
AgatheMazuret <agathe.mazuret@gmail.com>
2023-08-17 23:02:22 +0200

    First commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./style.css";
+
+
+const bandeau = () => {
+    return (
+        <div className="bandeau">
+            <div className="name">Agathe Mazuret</div>
+        </div>
+    );
+}
+
+export default bandeau;
