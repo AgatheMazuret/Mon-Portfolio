2dba903a3a64bc780ac259d52728a4f7c1171890
AgatheMazuret <agathe.mazuret@gmail.com>
2023-08-29 17:12:10 +0200

    Responsive texte

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -23,14 +23,15 @@ export default function Description() {
           <h1 className="text-4xl font-bold tracking-tight sm:text-4xl md:text-5xl lg:text-6xl" style={fontStylesTitle}>
             Agathe Mazuret
           </h1>
-          <p className="mt-6  text-lg leading-8 text-white">
-            Après avoir exploré le monde de la mode à travers mes études, j'ai rapidement réalisé que ma véritable passion résidait ailleurs.
-            {" "}
-            J'ai choisi de me reconvertir dans le développement web, une décision guidée par mon désir profond de créer, d'innover et de contribuer à l'univers numérique en constante évolution.
-            {" "}
-            Chaque ligne de code que je rédige est une toile sur laquelle je peins des solutions concrètes, faisant ainsi de ma passion mon métier. Cette reconversion m'a permis de fusionner ma créativité avec ma fascination pour la technologie.
-            
-          </p>
+          <p className="mt-6 text-sm md:text-base lg:text-lg leading-7 text-white">
+  Après avoir exploré le monde de la mode à travers mes études, j'ai rapidement réalisé que ma véritable passion résidait ailleurs.
+  {" "}
+  J'ai choisi de me reconvertir dans le développement web, une décision guidée par mon désir profond de créer, d'innover et de contribuer à l'univers numérique en constante évolution.
+  {" "}
+  Chaque ligne de code que je rédige est une toile sur laquelle je peins des solutions concrètes, faisant ainsi de ma passion mon métier. Cette reconversion m'a permis de fusionner ma créativité avec ma fascination pour la technologie.
+</p>
+
+
         </div>
       </div>
     </div>
