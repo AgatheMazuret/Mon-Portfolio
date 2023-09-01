e9dfc93aebb46fd8f39c83d7b433c230a4d9030d
AgatheMazuret <agathe.mazuret@gmail.com>
2023-09-01 13:44:29 +0200

    Rajout description

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -26,7 +26,7 @@ export default function Description() {
           <p className="mt-6 text-sm md:text-base lg:text-lg leading-7 text-white">
   Après avoir exploré le monde de la mode à travers mes études, j'ai rapidement réalisé que ma véritable passion résidait ailleurs.
   {" "}
-  J'ai choisi de me reconvertir dans le développement web, une décision guidée par mon désir profond de créer, d'innover et de contribuer à l'univers numérique en constante évolution.
+  J'ai choisi de me reconvertir dans le développement web en suivant une formation developpeur wev avec Openclassrooms, une décision guidée par mon désir profond de créer, d'innover et de contribuer à l'univers numérique en constante évolution.
   {" "}
   Chaque ligne de code que je rédige est une toile sur laquelle je peins des solutions concrètes, faisant ainsi de ma passion mon métier. Cette reconversion m'a permis de fusionner ma créativité avec ma fascination pour la technologie.
 </p>
